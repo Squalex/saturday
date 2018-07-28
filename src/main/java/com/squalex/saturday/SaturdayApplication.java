746effef7fc2badbc4db6ac7317246e9648033f8
Larry <larry.motalavigne@gmail.com>
2018-07-28 21:29:35 +0200

    Initial commit

---
+++ src/main/java/com/squalex/saturday/SaturdayApplication.java
@@ -0,0 +1,12 @@
+package com.squalex.saturday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaturdayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaturdayApplication.class, args);
+    }
+}
