f04d5ce2a8fa0b077e828f1e0ae297452f79427f
Larry <larry.motalavigne@gmail.com>
2018-07-29 21:31:11 +0200

    feat(first-step): Java Spring side done

--- src/main/java/com/squalex/saturday/SaturdayApplication.java
+++ src/main/java/com/squalex/saturday/SaturdayApplication.java
@@ -1,12 +1,47 @@
 package com.squalex.saturday;
 
+import com.squalex.saturday.model.Event;
+import com.squalex.saturday.model.Group;
+import com.squalex.saturday.model.GroupRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.stream.Stream;
 
 @SpringBootApplication
+//@EnableOAuth2Sso
+@AllArgsConstructor
 public class SaturdayApplication {
+
+    public final GroupRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(SaturdayApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+        return args -> {
+            Stream.of("Denver JUG", "Utah JUG", "Seattle JUG",
+                    "Richmond JUG").forEach(name ->
+                    repository.save(new Group(name))
+            );
+
+            Group djug = repository.findByName("Denver JUG");
+            Event e = Event.builder().title("Full Stack Reactive")
+                    .description("Reactive with Spring Boot + React")
+                    .date(Instant.parse("2018-12-12T18:00:00.000Z"))
+                    .build();
+            djug.setEvents(Collections.singleton(e));
+            repository.save(djug);
+
+            repository.findAll().forEach(System.out::println);
+        };
     }
 }
---
+++ src/main/java/com/squalex/saturday/model/GroupRepository.java
@@ -0,0 +1,7 @@
+package com.squalex.saturday.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+    Group findByName(String name);
+}
